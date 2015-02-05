c53fefb240272a2bfa9be2d28668ad7fd0e869a0
ypera006 <ypera006@fiu.edu>
2015-02-05 00:20:13 -0500

    usr interface vm request, still working on it

--- application/views/vm_request.php
+++ application/views/vm_request.php
@@ -4,11 +4,41 @@
 <h2> Virtual Machine Resources page under development </h2>
 
 <?php
-    if (isUserLoggedIn($this))
-    {
-        if (!isProfessor($this))
-           echo("<h4> NOTE: Virtual machine request must be done by team, no individual requests allow </h4>");
-    }
+    if (!isProfessor($this))
+       echo("<h4> NOTE: Virtual machine request must be done by team, no individual requests allow </h4>");
 ?>
-
+<div id="machines">
+    <div class="machine col-md-12">
+        <select name="role">
+            <?php 
+                $oses = array(
+                    "Windows Server 2008",
+                    "Ubuntu Server",
+                    "Windows Server 2003"
+                );
+                foreach($oses as $os)
+                    echo '<option>'.$os.'</option>';
+            ?>
+        </select>
+        
+        <select name="role">
+            <option>2</option>
+            <option>4</option>
+            <option>8</option>
+            <option>12</option>
+            <option>16</option>
+        </select>
+    </div>
+</div>
+<br>
+<button id="addRequest" type="button" class="btn btn-default">Add Request</button>
+<script>
+$('#addRequest').click(function(){
+    console.log("Clicked add request");
+    var machines = $("#machines");
+    var lastMachine = $("#machines .machine:last-child");
+    machines.append($('<br>'));
+    machines.append(lastMachine.clone());
+});
+</script>
 <?php $this->load->view("template_footer"); ?>
