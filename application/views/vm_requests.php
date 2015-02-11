079c593c6401b4e97bb5f826e2250bde34cc24ea
ypera006 <ypera006@fiu.edu>
2015-02-11 16:05:13 -0500

    added some functionality

--- application/views/vm_requests.php
+++ application/views/vm_requests.php
@@ -1,26 +1,10 @@
 <?php $this->load->view("template_header"); ?>
-<!--
-<?php 
-    function generateSelect($name = '', $options = array(), $default = '') {
-    $html = '<select name="'.$name.'">';
-    foreach ($options as $option => $value) {
-        if ($option == $default) {
-            $html .= '<option value='.$value.' selected="selected">'.$option.'</option>';
-        } else {
-            $html .= '<option value='.$value.'>'.$option.'</option>';
-        }
-    }
 
-    $html .= '</select>';
-    return $html;
-    //$html = generateSelect('os', $oses, $requests->OS); /*call statement*/
-}
-?>
--->
 <h1> VMs requests </h1>
+
 <div id="machines">
 <div class="machine col-md-12">
-<table class="auto table">
+<table class="auto table" id="machines_table">
     <thead>
         <tr>
             <th>Request No.</th>
@@ -34,45 +18,48 @@
     <tbody>
         <?php foreach($requests as $request):?>
         <tr>
-            <td><?php echo $request->id;?></td>
             <td>
-                <select name = "os" style="width: auto">
+                <select name="key" style="width: auto">
+                <?php 
+                   echo '<option>'.$request->id.'</option>'
+                ?>
+                </select>
+            </td>
+            <td>
+                <select name="os" style="width: auto">
                 <?php 
                     $oses = array(
-                        'Windows Server 2008'=>0,
-                        'Ubuntu Server'=>1,
-                        'Windows Server 2003'=>2
+                        'Windows Server 2008',
+                        'Ubuntu Server',
+                        'Windows Server 2003'
                     );
                         
-                        $os_i = 0; 
-                        foreach($oses as $os=>$d){
-                            $os_i++;
+                        foreach($oses as $os){
                             if($request->OS == $os)
-                                echo'<option value ='.$os_i.' selected="selected">'.$os.'</option>';
+                                echo'<option value ='.$os.' selected="selected">'.$os.'</option>';
                             else{
-                             echo'<option value="'.$os_i.'">'.$os.'</option>';
+                             echo'<option value="'.$os.'">'.$os.'</option>';
                             }
                         }
                 ?>
                 </select>
             </td>
             <td>
-                <select name = "ram" style="width: auto">
+                <select name="ram" style="width: auto">
                 <?php 
                     $rams = array(
-                        2=>0,
-                        4=>1,
-                        8=>2,
-                        12=>3,
-                        16=>4
+                        2,
+                        4,
+                        8,
+                        12,
+                        16
                     );
-                    $i = 0; 
-                        foreach($rams as $ram=>$d){
-                            $i++;
+                    
+                        foreach($rams as $ram){
                             if($request->RAM == $ram)
-                                echo'<option value ='.$i.' selected="selected">'.$ram.'</option>';
+                                echo'<option value ='.$ram.' selected="selected">'.$ram.'</option>';
                             else{
-                             echo'<option value="'.$i.'">'.$ram.'</option>';
+                             echo'<option value="'.$ram.'">'.$ram.'</option>';
                             }
                         }
                 ?>    
@@ -82,20 +69,19 @@
                 <select name="hdd" style="width: auto">
                 <?php
                         $hdds = array(
-                            8=>0,
-                            12=>1,
-                            16=>2,
-                            20=>3,
-                            24=>4,
-                            30=>5
+                            8,
+                            12,
+                            16,
+                            20,
+                            24,
+                            30
                         );
-                        $i = 0; 
-                            foreach($hdds as $hdd=>$d){
-                                $i++;
-                                if($request->numb_vm == $hdd)
-                                    echo'<option value ='.$i.' selected="selected">'.$hdd.'</option>';
+                        
+                            foreach($hdds as $hdd){
+                                if($request->storage == $hdd)
+                                    echo'<option value ='.$hdd.' selected="selected">'.$hdd.'</option>';
                                 else{
-                                 echo'<option value="'.$i.'">'.$hdd.'</option>';
+                                 echo'<option value="'.$hdd.'">'.$hdd.'</option>';
                                 }
                             }
                   ?>
@@ -105,23 +91,22 @@
                 <select name="qty" style="width: auto">
                     <?php
                         $vms = array(
-                            1=>0,
-                            2=>1,
-                            3=>2,
-                            4=>3,
-                            5=>4,
-                            6=>5,
-                            7=>6,
-                            8=>7,
-                            9=>8
+                            1,
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8,
+                            9
                         );
-                        $i = 0; 
-                            foreach($vms as $vm=>$d){
-                                $i++;
-                                if($request->storage == $vm)
-                                    echo'<option value ='.$i.' selected="selected">'.$vm.'</option>';
+                        
+                            foreach($vms as $vm){
+                                if($request->gty == $vm)
+                                    echo'<option value ='.$vm.' selected="selected">'.$vm.'</option>';
                                 else{
-                                 echo'<option value="'.$i.'">'.$vm.'</option>';
+                                 echo'<option value="'.$vm.'">'.$vm.'</option>';
                                 }
                             }
                     ?>
@@ -131,17 +116,16 @@
                 <select name="status" style="width: auto">
                     <?php 
                     $status = array(
-                            'PENDING'=>0,
-                            'APPROVED'=>1,
-                            'DENIED'=>2
+                            'PENDING',
+                            'APPROVED',
+                            'DENIED'
                         );
-                        $i = 0; 
-                            foreach($status as $st=>$d){
-                                $i++;
+                        
+                            foreach($status as $st){
                                 if($request->status == $st)
-                                    echo'<option value ='.$i.' selected="selected">'.$st.'</option>';
+                                    echo'<option value ='.$st.' selected="selected">'.$st.'</option>';
                                 else{
-                                 echo'<option value="'.$i.'">'.$st.'</option>';
+                                 echo'<option value="'.$st.'">'.$st.'</option>';
                                 }
                             }
                     ?>
@@ -160,29 +144,76 @@
   <input type="text" class="form-control" id="usr">
 </div>-->
 <label for="usr">email address:</label>
-<input  type="text"/>
+<input  type="text" class="form-control"/>
 <button id="submitRequests" type="button" class="btn btn-default pull-right">Submit</button>
+
+<script>
+$('#submitRequests').click(function(){
+    console.log("Clicked submit");
+    var data = getTableContent();
+    console.log("machines: ");
+    console.log(data);
+    uploadMachines(data);
+});
+
+function uploadMachines(machineList){
+    $.ajax({
+        type: "POST",
+        url: "./vm-request?projectid=82",
+        data: JSON.stringify(machineList),
+        dataType: "json",
+        success: function(response){
+            console.log("response");
+            console.log(response);
+        }
+    });
+}
+
+function getTableContent() {
+    var data = [];
+    var table = $('#machines_table tbody tr');
+    for(var i = 0 ; i< table.length;i++){
+        var row = table.eq(i);
+        var key = row.find('[name="key"]').val();
+        var os = row.find('[name="os"]').val();
+        var ram = row.find('[name="ram"]').val();
+        var hdd = row.find('[name="hdd"]').val();
+        var qty = row.find('[name="qty"]').val();
+        var status = row.find('[name="status"]').val();
+        var obj = {
+            "key":key,
+            "os":os,
+            "ram":ram,
+            "hdd":hdd,
+            "qty":qty,
+            "status":status
+        };
+        data.push(obj);
+    }
+    return data;
+}
+</script>
 
 <?php $this->load->view("template_footer"); ?>
 
-<!--
-                <select>
-                    <?php 
-                            $oses = array(
-                                "Windows Server 2008"=>1,
-                                "Ubuntu Server"=>2,
-                                "Windows Server 2003"=>3
-                            );
-                           $html = generateSelect('oses', $oses, $requests->OS);
-                        ?>
-                    
-                <?php echo $request->OS;?>
 
-    echo '<option value="'.$os.'"' ;
-   if(in_array('Windows Server 2008', $oses))
-       echo $requests->OS;
-   else
-       echo '>'.$os.'</option>';
-                    
-                </select>
+
+
+<!--
+<?php 
+    function generateSelect($name = '', $options = array(), $default = '') {
+    $html = '<select name="'.$name.'">';
+    foreach ($options as $option => $value) {
+        if ($option == $default) {
+            $html .= '<option value='.$value.' selected="selected">'.$option.'</option>';
+        } else {
+            $html .= '<option value='.$value.'>'.$option.'</option>';
+        }
+    }
+
+    $html .= '</select>';
+    return $html;
+    $html = generateSelect('os', $oses, $request->OS); /*call statement*/
+}
+?>
 -->
