45818f53f0550c0f5f30aa1fe86f208ddf6799b8
ypera006 <ypera006@fiu.edu>
2015-03-24 21:12:59 -0400

    #355 new interface for filter an image added

--- application/views/vm_images.php
+++ application/views/vm_images.php
@@ -39,44 +39,37 @@ echo form_close();
 </div>
 
 <?php
-  echo form_open('projectcontroller/filterImages', array(
+echo form_open('projectcontroller/filterImages', array(
                                                   'class' => '',
                                                   'id' => ''
                                                   ));
   ?>
+
 <div class="well" > 
 <div class="well" style=" margin-top: 20px; margin-bottom: 10px;">
 <div class="subsection" >
-    <h4> Filter Images Names </h4>
+    <h4> Filter Image Names </h4>
     
 <?php
-	echo( "<div style='margin-left: -10px'>" );
-	echo form_checkbox( array(
-                                'name' => 'all_images',
-                                'id' => 'all_images',
-                                'value' => 'all_images',
-                                'checked' => FALSE,
-                                'style' => 'margin: 10px' ) );
-	echo( "All Images" );
-	echo( "</div>" );
-	echo( "<div style='margin: -10px'>" );
-	echo form_checkbox( array( 
-                                'name' => 'active',
-                                'id' => 'status_active',
-                                'value' => 'active',
-                                'checked' => FALSE,
-                                'style' => 'margin: 10px' ) );
-	echo( "Status: ACTIVE" );
-	echo( "</div>" );
-	echo( "<div style='margin: -10px'>" );
-	echo form_checkbox( array( 
-                                'name' => 'inactive',
-                                'id' => 'status_inactive',
-                                'value' => 'inactive',
-                                'checked' => FALSE,
-                                'style' => 'margin: 10px' ) );
-	echo( "Status: INACTIVE" );
-	echo( "</div>" );
+	echo( "<div>" );
+
+        echo( "<br>" );
+        echo form_dropdown( 'id', 
+                array( 'all_images' => 'All Images', 
+                       'active' => 'ACTIVE',
+                       'inactive' => 'INACTIVE'),'id');
+        echo("<br>");
+        echo("<br>");
+        echo form_input(array(
+                      'id' => 'image_name',
+                      'name' => 'image_name',
+                      'type' => 'text',
+                      'placeholder' => 'Image Name',
+                      'style' => "margin-left: 1px; margin-right: 5px",
+                      'title' => 'Image Name'
+                      ));
+        
+	
 
 	echo( "<br>" );
 	echo form_submit(array(
@@ -85,57 +78,56 @@ echo form_close();
            'type' => 'Submit',
            'class' => 'btn btn-info',
            'value' => 'Search Filtered Images'
-       ));
-        echo form_close();
+        ));
+        echo( "</div>" );
+echo form_close();
         ?>
+
 </div>
 </div>    
     
-<table>
 <?php
-        echo( "<br>" );
         $image = "";
         $status = "";
-         foreach($images as $row){
-             $image = $row->image_name;
-             $status = $row->status;
-             
-             echo("<div class=\"well\">");
-             echo("<div class=\"text-center\">");
-                    
-                    echo form_open('projectcontroller/setImageStatus', array(
+        echo( "<br>" );
+        echo("<table class=\"table table-bordered\">");
+        echo("<tr>");
+            echo("<th>Image Name</th>");
+            echo("<th>Image Status</th>");
+            echo("<th>Change Image Status</th>");
+            echo("<th>Delete Image</th>");
+        echo("</tr>");
+        
+        foreach($images as $row){
+            echo("<tr>");
+                        echo form_open('projectcontroller/setImageStatus', array(
                                                                'class' => '',
                                                                'id' => ''
-                                                               ));  
-               echo('<h4> Image Name: '.$image.' </h4>');
-               if($status === 'INACTIVE'){
-                    echo('<h4>Image Status: <span class="label label-warning">'."$status".'</span></h4>');
-               }
-               if($status === 'ACTIVE'){
-                    echo('<h4>Image Status: <span class="label label-success">'."$status".'</span></h4>');
-               }
-                    $data = array( 
-                    'image_name' => $image,
-                    'status' => $status,
-                    'change_status' => TRUE
-                    );
-                    echo form_hidden($data);
-                    echo( "<br>" );
-                    echo form_submit(array(
-                       'id' => 'btn',
-                       'name' => 'search',
-                       'type' => 'Submit',
-                       'class' => 'btn btn-info',
-                       'value' => 'Set Image Status'
-                    )); 
+                                                               ));
+            $image = $row->image_name;
+            $status = $row->status;
+            echo("<td> $image </td>");
+            echo("<td> $status </td>");
+            echo("<td> change </td>");
+            echo("<td>");
+                 echo("content:\"\e014\"");
+            echo("</td>");
+            
+                        $data = array( 
+                            'image_name' => $image,
+                            'status' => $status,
+                            'change_status' => TRUE
+                            );
+                        echo form_hidden($data);
 
-                    echo form_close();
-                
-                echo("</div>");
-                echo("</div>");
-         }
+                        echo form_close(); 
+            
+            echo("</tr>");
+        }
+        
+        echo("</table>");
+        
 ?>
-    </table>
 </div>    
     
 
