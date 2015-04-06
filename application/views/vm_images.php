6db22f2f20f430043018dc3cacb75ba91ca33727
ypera006 <ypera006@fiu.edu>
2015-04-06 13:03:07 -0400

    #423 added html for filtering images

--- application/views/vm_images.php
+++ application/views/vm_images.php
@@ -38,105 +38,109 @@ echo form_close();
 ?>
 </div>
 
-<?php
-echo form_open('projectcontroller/filterImages', array(
-                                                  'class' => '',
-                                                  'id' => ''
-                                                  ));
-  ?>
+
 
 <div class="well" > 
-<div class="well" style=" margin-top: 20px; margin-bottom: 10px;">
-<div class="subsection" >
-    <h4> Filter Image Names </h4>
-    
-<?php
-	echo( "<div>" );
-
-        echo( "<br>" );
-        echo form_dropdown( 'id', 
-                array( 'all_images' => 'All Images', 
-                       'active' => 'ACTIVE',
-                       'inactive' => 'INACTIVE'),'id');
-        echo("<br>");
-        echo("<br>");
-        echo form_input(array(
-                      'id' => 'image_name',
-                      'name' => 'image_name',
-                      'type' => 'text',
-                      'placeholder' => 'Image Name',
-                      'style' => "margin-left: 1px; margin-right: 5px",
-                      'title' => 'Image Name'
-                      ));
-        
-	
-
-	echo( "<br>" );
-	echo form_submit(array(
-           'id' => 'btn',
-           'name' => 'search',
-           'type' => 'Submit',
-           'class' => 'btn btn-info',
-           'value' => 'Search Filtered Images'
-        ));
-        echo( "</div>" );
-echo form_close();
-        ?>
-
-</div>
-</div>    
-<?php
-        $image = "";
-        $status = "";
-        echo( "<br>" );
-        echo("<table id=\"images\" class=\"table table-bordered\">");
-        echo("<tr>");
-            echo("<th style=\"text-align:center\"> IMAGE NAME </th>");
-            echo("<th style=\"text-align:center\"> IMAGE STATUS </th>");
-            echo("<th style=\"text-align:center\"> CHANGE IMAGE STATUS </th>");
-            echo("<th style=\"text-align:center\"> DELETE IMAGE </th>");
-        echo("</tr>");
-        
-        foreach($images as $row){
-            echo("<tr>");
-                        
-            $image = $row->image_name;
-            $status = $row->status;
-            echo("<td style=\"text-align:center\">"); 
-                    echo $image ;
-            echo("</td>");
-            echo("<td style=\"text-align:center\">"); 
-                    echo $status;
-            echo("</td>");
-            echo("<td style=\"text-align:center\">");
-                if($status == 'ACTIVE'){
-                    echo("<a href=".base_url('./vm-images/status?status='.$status.
-                            "&image_name=".urlencode($image)).
+ 
+<br>
+    <table class="table table-bordered vm-img-table" id="image_table">
+        <thead>
+        <tr>
+            <th>      
+                <input id="image" class="input-medium text-filter" type="text" value="<?php echo $image ?>">
+            </th>
+            <th>  
+               <select class="field-custom dropdown" id="status">
+                  <?php echo '<option '.($status==='ALL STATUS'?'selected="selected"':'').' value="ALL STATUS">ALL STATUS</option>'; ?>
+                  <?php echo '<option '.($status==='ACTIVE'?'selected="selected"':'').' value="ACTIVE">ACTIVE</option>'; ?>
+                  <?php echo '<option '.($status==='INACTIVE'?'selected="selected"':'').'value="INACTIVE">INACTIVE</option>'; ?>
+                </select>
+            </th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+        <tr>
+            <th style="text-align:center"> IMAGE NAME </th>
+            <th style="text-align:center"> IMAGE STATUS </th>
+            <th style="text-align:center"> CHANGE STATUS </th>
+            <th style="text-align:center"> DELETE IMAGE </th>
+            <th style="text-align:center"> EDIT IMAGE </th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach($results as $row):?>
+        <tr>
+            <td style="text-align:center">
+                    <?php echo $row->image_name; ?>
+            </td>
+            <td style="text-align:center"> 
+                    <?php echo $row->status; ?>
+            </td>
+            <td style="text-align:center">
+            <?php
+                if($row->status == 'ACTIVE'){
+                    echo("<a href=".base_url('./vm-images/status?status='.$row->status.
+                            "&image_name=".urlencode($row->image_name)).
                             "> <img id=\"\" src=".base_url('img/green_light.png')." height=\"20\" width=\"20\" > </a>");
                 }else{
-                    echo("<a href=".base_url('./vm-images/status?status='.$status.
-                            "&image_name=".urlencode($image)).
+                    echo("<a href=".base_url('./vm-images/status?status='.$row->status.
+                            "&image_name=".urlencode($row->image_name)).
                             "> <img id=\"\" src=".base_url('img/red_light.png')." height=\"20\" width=\"20\" > </a>");
                 }
-            echo("</td>");
-            echo("<td style=\"text-align:center\">"); 
-                    $msg = "Are you sure you want to delete image $image ?";
+            ?>    
+            </td>
+            <td style="text-align:center">
+            <?php
+                    $msg = "Are you sure you want to delete image $row->image_name ?";
                     echo("<a href=".base_url('./vm-images/delete?image_name='.
-                            urlencode($image))." onclick=\"return confirm('$msg')\"> <img id=\"\" src=".
+                            urlencode($row->image_name))." onclick=\"return confirm('$msg')\"> <img id=\"\" src=".
                             base_url('img/deletered.png')." height=\"20\" width=\"20\" > </a>");
-            echo("</td>");
-            
-            echo("</tr>");
-        }
-        
-        echo("</table>");
-        
-?>  
+            ?>
+            </td>
+            <td style="text-align:center">
+            <?php
+                    echo("<a href=".base_url('./vm-images/edit?status='.$row->status.
+                            "&image_name=".urlencode($row->image_name)).
+                            "> <img id=\"\" src=".base_url('img/write.png')." height=\"20\" width=\"20\" > </a>");
+            ?>
+            </td>
+        </tr>
+        <?php endforeach;?>
+        </tbody>
+        </table>
 </div>    
-<script language="javascript" type="text/javascript">  
-//<![CDATA[  
-    var tf1 = setFilterGrid("images");  
-//]]>  
+<script>
+function filterForm(){
+
+    var status = $('#status').val();
+    var image = $("#image").val();
+    
+    var whereto = "./vm-images?";
+    
+    if(image!=='') whereto+="image="+image+"&";
+    if(status!=='') whereto+="status="+status+"&";
+
+    var lastChar = whereto.charAt(whereto.length-1);
+    console.log("last char: "+lastChar);
+    if(lastChar==='&'){
+        console.log("in here");
+        whereto = whereto.substring(0,whereto.length-1);
+    }
+    console.log(whereto);
+    window.location.href = whereto;
+}
+
+$('.text-filter').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        filterForm();
+    }
+});
+
+$(".dropdown" ).change(function() {
+    filterForm();
+});    
 </script>   
 
 <?php $this->load->view("template_footer"); ?>
