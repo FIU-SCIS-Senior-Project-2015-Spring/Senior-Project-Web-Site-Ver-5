ef031d0f1bb28ecd4ef31585925a13fafeea6c61
ypera006 <ypera006@fiu.edu>
2015-04-06 12:57:33 -0400

    #423 added route for edit image method

--- application/config/routes.php
+++ application/config/routes.php
@@ -79,6 +79,7 @@ $route['(?i)(vm-images)'] = 'projectcontroller/vm_images';
 $route['(?i)(vm-images)/addImage'] = 'projectcontroller/addImages';
 $route['(?i)(vm-images)/status'] = 'projectcontroller/changeImageStatus';
 $route['(?i)(vm-images)/delete'] = 'projectcontroller/deleteImage';
+$route['(?i)(vm-images)/edit'] = 'projectcontroller/loadEditImage';
 
 $route['(?i)(past-projects)'] = 'projectcontroller/past_projects';
 $route['(?i)(project)/(create)'] = 'projectcontroller/create_new_project';
